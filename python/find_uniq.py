f65f77fce7b23b4e6a60927551f934b509be1813
Federico Ciminelli <76215075+federicociminelli@users.noreply.github.com>
2021-03-10 16:14:06 +0100

    Update find_uniq.py
    Entrambe le soluzioni funzionano, ma falliscono per TIMEOUT.

--- python/find_uniq.py
+++ python/find_uniq.py
@@ -1,13 +1,10 @@
-
-
 '''
 There is an array with some numbers. All numbers are equal except for one. Try to find it!
 It’s guaranteed that array contains at least 3 numbers.
 The tests contain some very huge arrays, so think about performance.
 '''
 
-
-# Funzionano entrambe ma non sono abbsatanza efficienti e performanti :(
+#Funzionano ma vanno in Timeout Fail su codeWars, da migliorare performance
 
 def find_uniq(arr):
     return [elem for elem in arr if arr.count(elem) == 1][0]
