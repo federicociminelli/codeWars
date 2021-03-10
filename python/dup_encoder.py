c48491193414b04afc185e95d5fb48253763a8e6
Federico Ciminelli <76215075+federicociminelli@users.noreply.github.com>
2021-03-10 16:11:45 +0100

    Update dup_encoder.py

--- python/dup_encoder.py
+++ python/dup_encoder.py
@@ -1,5 +1,3 @@
-
-
 '''
 The goal of this exercise is to convert a string to a new string where each character 
 in the new string is "(" if that character appears only once in the original string, 
