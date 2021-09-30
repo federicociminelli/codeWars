870a5034909d32579739ff7cf4020f053077329c
Federico Ciminelli <76215075+federicociminelli@users.noreply.github.com>
2021-09-30 15:49:13 +0200

    Create prefill.py

---
+++ python/prefill.py
@@ -0,0 +1,42 @@
+'''
+Create the function prefill that returns an array of n elements that all have the same value v. See if you can do this without using a loop.
+
+You have to validate input:
+
+v can be anything (primitive or otherwise)
+if v is ommited, fill the array with undefined
+if n is 0, return an empty array
+if n is anything other than an integer or integer-formatted string (e.g. '123') that is >=0, throw a TypeError
+When throwing a TypeError, the message should be n is invalid, where you replace n for the actual value passed to the function.
+
+Code Examples
+
+    prefill(3,1) --> [1,1,1]
+    
+    prefill(2,"abc") --> ['abc','abc']
+    
+    prefill("1", 1) --> [1]
+    
+    prefill(3, prefill(2,'2d'))
+      --> [['2d','2d'],['2d','2d'],['2d','2d']]
+      
+    prefill("xyz", 1)
+      --> throws TypeError with message "xyz is invalid"
+'''
+
+def prefill(n,v = None):
+    if v is None:
+        v = 'undefined'
+    if n == 0:
+        return []
+    if type(n) is str:
+        if n.isdigit():
+            n = int(n)
+            return [v]*int(n)
+        else:
+            raise TypeError(f'{n} is invalid')
+    if n is None or type(n) is float or type(n) is bool:
+        raise TypeError(f'{n} is invalid')
+    return [v]*n
+    
+    
