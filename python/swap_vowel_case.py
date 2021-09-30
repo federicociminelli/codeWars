2f68500ab499a46fcc1797689e2ae1e095fd5df2
Federico Ciminelli <76215075+federicociminelli@users.noreply.github.com>
2021-09-30 15:45:57 +0200

    Create swap_vowel_case.py

---
+++ python/swap_vowel_case.py
@@ -0,0 +1,20 @@
+'''
+Special thanks to SteffenVogel_79 for the idea.
+
+Challenge:
+
+Given a string, return a copy of the string with the vowels' case swapped.
+
+For this kata, assume that vowels are in the set "aeouiAEOUI".
+
+Example: Given a string "C is alive!", your function should return "C Is AlIvE!"
+
+Addendum: Your solution is only required to work for the ASCII character set.
+
+Please make sure you only swap cases for the vowels.
+'''
+
+def swap_vowel_case(st):
+    swap = str.maketrans('aeiouAEIOU', 'AEIOUaeiou')
+    return st.translate(swap)
+    
